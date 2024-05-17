191f7877f969c91f6b29f05c3c7558d0f7aa4496
AlesciDavide <davi.alesci@email.com>
2024-05-17 15:39:52 +0200

    bonus aggiunta difficoltà

--- js/script.js
+++ js/script.js
@@ -3,22 +3,40 @@ const startGameButton = document.querySelector('button#startGame');
 
 
 startGameButton.addEventListener('click', function(){
+
+    let sceltaDifficoltaEl = document.getElementById("sceltaDifficolta")
+    let difficulty = sceltaDifficoltaEl.value;
+    let valueClass = '';
+    if(difficulty == "semplice"){
+        difficulty = 100;
+        valueClass = 'elements_semplice';
+    }else if(difficulty == "normale"){
+        difficulty = 81;
+        valueClass = 'elements_normale';
+    }else{
+        difficulty = 49;
+        valueClass = 'elements_difficile';
+
+    }
+
     const checkcontainerEl = document.getElementById('container');
         if(checkcontainerEl !== null){
             checkcontainerEl.remove();
         }
+
     const containerEl = document.createElement('section');
     containerEl.id= 'container';
     const myMainEl = document.querySelector('main')
     myMainEl.appendChild(containerEl);
     const mycontainerEl = document.querySelector('#container');
-        for(let i = 0; i < 100; i++){
+        for(let i = 0; i < difficulty; i++){
             let divElements = document.createElement('div');
-            divElements.classList.add('elements');
+            divElements.classList.add(valueClass);
             divElements.append(i+1);
             mycontainerEl.appendChild(divElements);
             divElements.addEventListener('click', function(){
                 divElements.classList.add('asd');
+                console.log(i+1);
             })
         }
     
